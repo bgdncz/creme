6c0b2df98900f76817483a5f6f6ce69f519737ef
Bogdan Cuza <bogdancuza@protonmail.com>
2022-07-31 18:57:15 +0300

    Add login button and more info for product

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,16 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 
 const main = document.querySelector("main");
+const loginBtn = document.querySelector("#login-btn");
+let loggedIn = false;
 
 function createProduct(product) {
     const productHtml = `
     <div class="product">
         <img src=${product.img_url}/>
+        <div class="extra-info">
+            <h3>${product.name}</h3>
+        </div>
     </div>`
     main.innerHTML += productHtml;
 }
@@ -15,3 +20,14 @@ fetch("/products").then(res => res.json()).then(products => {
         createProduct(product);
     });
 });
+
+function handleLogin(clickEvent) {
+    if (loggedIn == false) {
+        loginBtn.textContent = "Log out";
+    } else {
+        loginBtn.textContent = "Log in";
+    }
+    loggedIn = !loggedIn;
+}
+
+loginBtn.addEventListener("click", handleLogin);
