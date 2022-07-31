4724abae0e4b6f615008d49c77768c0adec6c44a
Bogdan Cuza <bogdancuza@protonmail.com>
2022-07-31 12:52:15 +0300

    Seed database and render json for /products

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+Product.delete_all
+
+(1..20).each do |product|
+    name = Faker::Commerce.product_name
+    link = "https://google.com/"
+    price = 10.5
+    description = Faker::Lorem.words(number: 5).join(" ")
+    img_url = "https://phonlo.com/wp-content/uploads/2022/05/iphone11-select-2019-family_GEO_EMEA.jpg"
+    Product.create(name: name, link: link, price: price, description: description, img_url: img_url)
+end
