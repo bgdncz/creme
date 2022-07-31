2e74d4735b5b9f279789b4098644dd3f6265c6cb
Bogdan Cuza <bogdancuza@protonmail.com>
2022-07-31 16:32:58 +0300

    Add basic styling and display products

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,17 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+
+const main = document.querySelector("main");
+
+function createProduct(product) {
+    const productHtml = `
+    <div class="product">
+        <img src=${product.img_url}/>
+    </div>`
+    main.innerHTML += productHtml;
+}
+
+fetch("/products").then(res => res.json()).then(products => {
+    products.forEach(product => {
+        createProduct(product);
+    });
+});
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,6 @@ Product.delete_all
     link = "https://google.com/"
     price = 10.5
     description = Faker::Lorem.words(number: 5).join(" ")
-    img_url = "https://phonlo.com/wp-content/uploads/2022/05/iphone11-select-2019-family_GEO_EMEA.jpg"
+    img_url = "/assets/product.jpg"
     Product.create(name: name, link: link, price: price, description: description, img_url: img_url)
 end
