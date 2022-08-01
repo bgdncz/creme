edb1d16e55c97ce3df94da8897bf89c38a2205df
Bogdan Cuza <bogdancuza@protonmail.com>
2022-08-01 19:48:40 +0300

    Add profile images to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_01_093904) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_01_164517) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.string "link"
@@ -37,6 +37,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_01_093904) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "profile_img"
   end
 
   add_foreign_key "reviews", "products"
